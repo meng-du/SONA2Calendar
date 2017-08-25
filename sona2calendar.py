86af43ccdd9f5f8ecec071b3313331a85d6ae235
MetaD <mengdu@umich.edu>
2017-08-25 00:03:40 -0700

    Update sona2calendar.py

--- sona2calendar.py
+++ sona2calendar.py
@@ -52,35 +52,6 @@ def get_credentials():
         print('Storing credentials to ' + credential_path)
     return credentials
 
-def listMessagesMatchingQuery(service, user_id, query=''):
-    """List all Messages of the user's mailbox matching the query.
-
-    Args:
-    service: Authorized Gmail API service instance.
-    user_id: User's email address. The special value "me"
-    can be used to indicate the authenticated user.
-    query: String used to filter messages returned.
-    Eg.- 'from:user@some_domain.com' for Messages from a particular sender.
-
-    Returns:
-    List of Messages that match the criteria of the query. Note that the
-    returned list contains Message IDs, you must use get with the
-    appropriate ID to get the details of a Message.
-    """
-    response = service.users().messages().list(userId=user_id,
-                                               q=query).execute()
-    messages = []
-    if 'messages' in response:
-      messages.extend(response['messages'])
-
-    while 'nextPageToken' in response:
-      page_token = response['nextPageToken']
-      response = service.users().messages().list(userId=user_id, q=query,
-                                         pageToken=page_token).execute()
-      messages.extend(response['messages'])
-
-    return messages
-
 
 if __name__ == '__main__':
     try:
