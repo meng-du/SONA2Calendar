824e8f5e3c7094ceddb4daa984990257cda868f0
MetaD <mengdu@umich.edu>
2017-08-26 01:05:47 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ query_term = 'subject:"Upcoming Study Reminder"'
 # how each study is formatted in the SONA reminder emails
 line_beginnings = ['Study: ', 'Date: ', 'Location: ', 'Participant: ']
 # google calendar id (find it in your Calendar Settings -> Calendar Address)
-google_calendar_id = 'vvfa71to3uhdij1lkm6ueqkelg@group.calendar.google.com'
+google_calendar_id = 'example@group.calendar.google.com'
 # the event name to be added to the google calendar
 # change this to an empty string if you want to use the same study name as in the SONA system
 alternative_study_name = 'Faces Study'
